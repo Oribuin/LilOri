da93333782cb754af16f9dc3e3004840c7e63625
Robin <rjmullineux@gmail.com>
2023-01-08 03:14:58 +0000

    Temp Remove SubCommand

--- src/main/java/xyz/oribuin/lilori/handler/SubCommand.java
+++
@@ -1,9 +0,0 @@
-package xyz.oribuin.lilori.handler;
-
-public abstract class SubCommand {
-
-    private final String name;
-    private final String description;
-    private final String usage;
-
-}
